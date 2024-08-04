a03f40ac7104564a074ef1912750a32f65e46826
dumurzakov38 <danilumurzakov38@gmail.com>
2024-08-04 10:41:15 +0500

    error fixing

--- Shop.Client/app/src/App.tsx
+++ Shop.Client/app/src/App.tsx
@@ -2,9 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React, { FC } from "react";
 import "./App.css";
 
-import { Nav } from "./components/NavPage/FNav";
-import { Main } from "./components/MainPage/FMain";
-import { Product } from "./components/ProductsPage/FProduct";
+import { Nav } from "./components/NavPage/Nav";
+import { Main } from "./components/MainPage/Main";
+import { Product } from "./components/ProductsPage/Product";
 import { ProductsList } from "./components/ProductsPage/ProductsList";
 
 const App: FC = () => {
